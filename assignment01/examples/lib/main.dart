42836a51c055656d4aabc6f3a91d3e697f97e625
jaume <jaume.montagut.i.guix@gmail.com>
2019-10-21 23:53:46 +0200

    testing images

--- assignment01/examples/lib/main.dart
+++ assignment01/examples/lib/main.dart
@@ -9,23 +9,20 @@ class ShopApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            color: Colors.yellow,
-            // child : Icon(
-            //   Icons.arrow_back_ios,
-            //   color : Colors.pink,
-            //   size : 24.0
-            // )
-            // child: Icon(
-            //   Icons.shopping_cart,
-            //   color: Colors.pink,
-            //   size: 24.0,
-            //   semanticLabel: 'Text to announce in acessibility modes',
-            // ),
-          ),
+          body: Image.asset('assets/clothes.png'),
         ),
-      ),
     );
   }
 }
+
+
+// child: Icon(
+//   Icons.arrow_back_ios,
+//   color: Colors.pink,
+//   size: 24.0)
+// child: Icon(
+//   Icons.shopping_cart,
+//   color: Colors.pink,
+//   size: 24.0,
+//   semanticLabel: 'Text to announce in acessibility modes',
+// ),

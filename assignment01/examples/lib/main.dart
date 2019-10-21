7f46b07d2145b69f95ff85e301518e051b7e761d
jaume <jaume.montagut.i.guix@gmail.com>
2019-10-22 00:46:37 +0200

    adding bottom window

--- assignment01/examples/lib/main.dart
+++ assignment01/examples/lib/main.dart
@@ -9,12 +9,37 @@ class ShopApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Image.asset('assets/clothes.png'),
+        body: Stack(
+          children: <Widget>[
+            Image.asset(
+              'assets/clothes.png',
+            ),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 4,
+                  child: Container(),
+                  //TOREMOVE: Transparent container, a litte bit dirty
+                ),
+                Expanded(
+                    flex: 6,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                    ))
+              ],
+            )
+          ],
         ),
+      ),
     );
   }
 }
-
 
 // child: Icon(
 //   Icons.arrow_back_ios,

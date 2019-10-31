5196e8f6e09a9082fbd98c4fc90ef9a68ce1a510
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2019-10-31 15:25:26 +0100

    exercice 3

---
+++ ExamPractice/examples/lib/Counter.dart
@@ -0,0 +1,6 @@
+class Counter{
+  String word;
+  int count = 0;
+
+  Counter(this.word);
+}
--- ExamPractice/examples/lib/main.dart
+++ ExamPractice/examples/lib/main.dart
@@ -7,8 +7,28 @@ void main() {
 class ExamenPractice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(home: MainWindow());
+  }
+}
+
+class MainWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.count(
+        crossAxisCount: 5,
+        children: List.generate(
+          100,
+          (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }

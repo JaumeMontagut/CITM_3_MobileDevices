330edb8adb592c1e8a434d829268bdcaa0d48eda
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2019-10-31 15:13:05 +0100

    creating flutter project

---
+++ ExamPractice/examples/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ExamenPractice());
+}
+
+class ExamenPractice extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
